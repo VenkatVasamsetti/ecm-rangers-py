ed84b4b725c6cf9de4f225c9f2eb32438681c967
Venkat V <venkat.vasamsetti1@gmail.com>
2024-03-17 11:23:24 +0530

    added flask app

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.errorhandler(Exception)
+def handle_error(e):
+    code = 500
+    if isinstance(e, HTTPException):
+        code = e.code
+    return jsonify(error=str(e)), code
+
+stores = [   {
+        "name": "My Store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_stores():
+    return {"stores": stores}
+
+
+app.run(debug=True)
