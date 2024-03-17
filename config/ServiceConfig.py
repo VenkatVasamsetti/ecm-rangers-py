2bfed7d880b4de07c3af97c38cbd75e0576d2b2a
Venkat V <venkat.vasamsetti1@gmail.com>
2024-03-18 01:30:38 +0530

    added models,service,config

--- config/ServiceConfig.py
+++ config/ServiceConfig.py
@@ -1,5 +1,4 @@
 serverconfig = {
-    "STORAGE_ACCOUNT_KEY" : "wvjaKDw3qX9W49n3g9GHsqH3YTpomkHKBBlSWBRdsSAzSeBHXwZE",
     "INDEX_NAME" : "azureblob-index",
     "SEARCH_ENDPOINT" : "https://aitestcvsearch.search.windows.net",
     "CONTAINER_NAME" : "",
