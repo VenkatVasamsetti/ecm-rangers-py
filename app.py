8a72bb26d735d6cde8b8204b62f042777fbe62dc
Venkat V <venkat.vasamsetti1@gmail.com>
2024-03-17 11:57:53 +0530

    updated controller

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask.logging import create_logger
+import logging
+
 
 app = Flask(__name__)
+
 app.config["DEBUG"] = True
 
+#Logging and tracing
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
+
+#Handling globle exceptions
 @app.errorhandler(Exception)
 def handle_error(e):
     code = 500
@@ -21,9 +30,18 @@ stores = [   {
     }
 ]
 
+
+# REST services
 @app.get("/store")
 def get_stores():
     return {"stores": stores}
 
 
-app.run(debug=True)
+
+
+
+
+#Application running
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
