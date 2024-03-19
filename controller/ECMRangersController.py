26ff7faaacd03d58726993e589ab4258be1d0948
Venkat V <venkat.vasamsetti1@gmail.com>
2024-03-19 13:22:37 +0530

    updated default response

--- controller/ECMRangersController.py
+++ controller/ECMRangersController.py
@@ -7,6 +7,8 @@ from flask.logging import create_logger
 import logging
 from service.ECMResumeAnalysis import ECMResumeAnalysis
 
+from model.MatchResponse import MatchResponse
+
 #Logging and tracing
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
@@ -26,14 +28,22 @@ def searchBestMatch():
     LOG.debug('------search method----')
     content_type = request.headers.get('Content-Type')
     if content_type == 'application/json':
-        LOG.debug('------content type json----')
         data = json.loads(request.data)
+        if data['category'] != 'resume':
+            LOG.debug('Category is not matching')
+            mr = MatchResponse('Bad Request : category is not matching. it should be resume',0, None, None)
+            return defaultResp(mr)
+        LOG.debug('------content type json----')       
         return azObj.searchTfidVet(data['context'], data['threshold'], data['noOfMatches'])
     else:
         LOG.debug('------content type not json----')
         return azObj.processData('test')
 @app.get('/ping')
 def pingService():
-    return azObj.processData('test')
+    mr = MatchResponse('healthy',0, None, None)
+    return defaultResp(mr)
+
+def defaultResp(obj):
+    return json.dumps(obj,default=lambda o: o.__dict__)
 print("-----Cntroller end-----",app)
 
